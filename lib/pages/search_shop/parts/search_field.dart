2380c5ef6ace5ae28f3d7ea7302f2ffd20223ee1
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-05-21 15:22:17 +0500

    feat: add search shop field

---
+++ lib/pages/search_shop/parts/search_field.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:skar/helpers/functions.dart';
+import 'package:skar/helpers/static_data.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class ShopSearchField extends StatelessWidget {
+  ShopSearchField({super.key});
+
+  final TextEditingController searchShopCtrl = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 40,
+      width: screenProperties(context).width - 70,
+      child: TextField(
+        controller: searchShopCtrl,
+        // focusNode: focusNode,
+        textInputAction: TextInputAction.search,
+        textAlignVertical: TextAlignVertical.center,
+        cursorColor: elevatedButtonColor,
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.only(top: 0, left: 20),
+          hintText: AppLocalizations.of(context)!.searchShop,
+          filled: true,
+          fillColor: Colors.white,
+          enabledBorder: outlinedInputBorder(),
+          focusedBorder: outlinedInputBorder(),
+          suffixIcon: IconButton(
+            onPressed: () {},
+            // onPressed: () {
+            //   if (rightSize == minRightSize) {
+            //     ref.read(rightSizeProvider.notifier).state = maxRightSize;
+            //     FocusScope.of(context).requestFocus(focusNode);
+            //     return;
+            //   }
+            //   ref.read(rightSizeProvider.notifier).state = minRightSize;
+            //   FocusScope.of(context).unfocus();
+            // },
+            icon: Icon(Icons.search, color: elevatedButtonColor),
+          ),
+        ),
+        // onSubmitted: (value) async {
+        //   await ref.read(markersProvider.notifier).removeAllMarkers();
+        //   ref.read(shopParamProvider.notifier).changeSearch(value);
+        // },
+      ),
+    );
+  }
+}
+
+OutlineInputBorder outlinedInputBorder() {
+  return OutlineInputBorder(
+    borderSide: BorderSide(color: elevatedButtonColor),
+    borderRadius: const BorderRadius.all(Radius.circular(20)),
+  );
+}
--- lib/pages/search_shop/search_shop.dart
+++ lib/pages/search_shop/search_shop.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:skar/helpers/functions.dart';
+import 'package:skar/helpers/static_data.dart';
+import 'package:skar/pages/search_shop/parts/search_field.dart';
 
 class SearchShopPage extends StatelessWidget {
   const SearchShopPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Search Shop'),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding:
+            EdgeInsets.only(top: screenProperties(context).topSafeArea + 5),
+        child: Row(
+          children: [
+            IconButton(
+              onPressed: () => Navigator.pop(context),
+              icon: Icon(Icons.arrow_back, color: elevatedButtonColor),
+            ),
+            ShopSearchField(),
+          ],
+        ),
       ),
     );
   }
