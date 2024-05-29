714102e688744f6446de127b5653acaf03943742
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-05-29 17:58:08 +0500

    feat: add FavoriteShops part of favorite page

--- lib/pages/favorites/favorites.dart
+++ lib/pages/favorites/favorites.dart
@@ -3,19 +3,32 @@ import 'package:skar/helpers/functions.dart';
 import 'package:skar/pages/favorites/parts/favorite_tab_bar.dart';
 
 class LikesPage extends StatelessWidget {
-  const LikesPage({super.key});
+  LikesPage({super.key});
+
+  final PageController _pageController = PageController();
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.only(top: screenProperties(context).topSafeArea + 10),
-      child: const Column(
+      child: Column(
         children: [
-          Row(
+          const Row(
             children: [
               FavoritePageTabBar(text: 'Dukan'),
               FavoritePageTabBar(text: 'Haryt'),
             ],
+          ),
+          PageView.builder(
+            controller: _pageController,
+            itemBuilder: (BuildContext context, int index) {},
+            // onPageChanged: (value) {
+            //   ref.read(activeImageColorProvider.notifier).state = value;
+            // },
+            // itemCount: productColor.images!.length,
+            // itemBuilder: (context, index) {
+            //   return showCachImageMethod(productColor.images![index]);
+            // },
           ),
         ],
       ),
