d1c13a5a6e195ea29f966af3be593c553474b61f
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-05-29 18:12:58 +0500

    feat: add activeFavoriteProvider provider

--- lib/pages/favorites/favorites.dart
+++ lib/pages/favorites/favorites.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:skar/helpers/functions.dart';
 import 'package:skar/pages/favorites/parts/favorite_tab_bar.dart';
 import 'package:skar/pages/favorites/parts/products.dart';
 import 'package:skar/pages/favorites/parts/shops.dart';
+import 'package:skar/providers/pages/favorites.dart';
 
-class FavoritesPage extends StatelessWidget {
+class FavoritesPage extends ConsumerWidget {
   FavoritesPage({super.key});
 
   final PageController _pageController = PageController(initialPage: 0);
   final List<Widget> _pages = [const FavoriteShops(), const FavoriteProducts()];
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Padding(
       padding: EdgeInsets.only(top: screenProperties(context).topSafeArea + 10),
       child: Column(
@@ -26,6 +28,9 @@ class FavoritesPage extends StatelessWidget {
           ),
           Expanded(
             child: PageView.builder(
+              onPageChanged: (value) {
+                ref.read(activeFavoriteProvider.notifier).state = value;
+              },
               controller: _pageController,
               itemCount: _pages.length,
               itemBuilder: (BuildContext context, int index) {
