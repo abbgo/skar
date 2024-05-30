651daca169c7898eb6650a2806e611071ecf75c9
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-05-30 18:11:52 +0500

    fix: update favorites page

--- lib/pages/favorites/favorites.dart
+++ lib/pages/favorites/favorites.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:skar/helpers/static_data.dart';
 import 'package:skar/pages/favorites/parts/products.dart';
 import 'package:skar/pages/favorites/parts/shops.dart';
@@ -13,7 +12,6 @@ class FavoritesPage extends StatefulWidget {
 
 class _FavoritesPageState extends State<FavoritesPage>
     with TickerProviderStateMixin {
-  final List<Widget> _pages = [const FavoriteShops(), const FavoriteProducts()];
   late TabController _tabController;
 
   @override
@@ -30,32 +28,28 @@ class _FavoritesPageState extends State<FavoritesPage>
 
   @override
   Widget build(BuildContext context) {
-    return Consumer(
-      builder: (context, ref, child) {
-        return Scaffold(
-          appBar: AppBar(
-            toolbarHeight: 0,
-            bottom: TabBar(
-              labelPadding: const EdgeInsets.only(bottom: 10),
-              labelStyle: const TextStyle(fontWeight: FontWeight.bold),
-              labelColor: elevatedButtonColor,
-              indicatorColor: elevatedButtonColor,
-              controller: _tabController,
-              indicatorWeight: 2,
-              dividerColor: Colors.white,
-              overlayColor: WidgetStateColor.transparent,
-              tabs: const [
-                Text('Dukan'),
-                Text('Haryt'),
-              ],
-            ),
-          ),
-          body: TabBarView(
-            controller: _tabController,
-            children: _pages,
-          ),
-        );
-      },
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 0,
+        bottom: TabBar(
+          labelPadding: const EdgeInsets.only(bottom: 10),
+          labelStyle: const TextStyle(fontWeight: FontWeight.bold),
+          labelColor: elevatedButtonColor,
+          indicatorColor: elevatedButtonColor,
+          controller: _tabController,
+          indicatorWeight: 2,
+          dividerColor: Colors.white,
+          overlayColor: WidgetStateColor.transparent,
+          tabs: const [
+            Text('Dukan'),
+            Text('Haryt'),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: const [FavoriteShops(), FavoriteProducts()],
+      ),
     );
   }
 }
