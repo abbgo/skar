7e3b05c88790d0b089e138fc406f3909efad489e
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-06-01 15:41:32 +0500

    feat: add hasFavoriteProductsProvider provider and update FavoriteProducts part of favorites

--- lib/pages/favorites/parts/products.dart
+++ lib/pages/favorites/parts/products.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:skar/pages/favorites/parts/no_favorites.dart';
+import 'package:skar/providers/params/product_param.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class FavoriteProducts extends StatelessWidget {
+class FavoriteProducts extends ConsumerWidget {
   const FavoriteProducts({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Center(child: Text('Products'));
+  Widget build(BuildContext context, WidgetRef ref) {
+    bool hasFavoriteProducts = ref.watch(hasFavoriteProductsProvider);
+
+    return !hasFavoriteProducts
+        ? NoFavorites(text: AppLocalizations.of(context)!.noFavoriteProducts)
+        : const Center(child: Text('has data'));
   }
 }
--- lib/providers/params/product_param.dart
+++ lib/providers/params/product_param.dart
@@ -2,3 +2,5 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 var productSearchProvider = StateProvider.autoDispose<String>((ref) => '');
 var hasProductsProvider = StateProvider.autoDispose<bool>((ref) => true);
+var hasFavoriteProductsProvider =
+    StateProvider.autoDispose<bool>((ref) => true);
