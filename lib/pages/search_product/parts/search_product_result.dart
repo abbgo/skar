dcb2d0f71263946d4dcc2aad8dcf4378c8af0130
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-06-03 07:14:26 +0500

    fix: update SearchProductResult widget

--- lib/pages/search_product/parts/search_product_result.dart
+++ lib/pages/search_product/parts/search_product_result.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:skar/pages/parts/error.dart';
+import 'package:skar/providers/params/product_param.dart';
 
-class SearchProductResult extends StatelessWidget {
+class SearchProductResult extends ConsumerWidget {
   const SearchProductResult({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Expanded(
-      child: Center(
-        child: Text('rasult products'),
-      ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    String search = ref.watch(productSearchProvider);
+    bool hasProducts = ref.watch(hasProductsProvider);
+
+    return Expanded(
+      child: search != ''
+          ? !hasProducts
+              ? const NoResult()
+              : const Center(
+                  child: Text('rasult products'),
+                )
+          : const SizedBox(),
     );
   }
 }
