c1f3c6d0e386b765bb615c2661c57c566239bb6d
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-06-07 11:15:31 +0500

    fix: update ShopPageButtons part widget

--- lib/pages/shop/parts/shop_buttons.dart
+++ lib/pages/shop/parts/shop_buttons.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:skar/helpers/functions.dart';
 import 'package:skar/methods/pages/shop.dart';
 import 'package:skar/models/shop.dart';
 import 'package:skar/pages/shop/parts/product_search_field.dart';
 import 'package:skar/pages/shop/parts/shop_favorite_button.dart';
 import 'package:skar/providers/pages/shop.dart';
+import 'package:skar/styles/colors.dart';
 
 class ShopPageButtons extends ConsumerWidget {
   const ShopPageButtons({super.key, required this.shop});
@@ -14,27 +16,43 @@ class ShopPageButtons extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     bool openSearchBar = ref.watch(openSearchBarProvider);
+    bool isLightBrightness = screenProperties(context).isLightBrightness;
 
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         IconButton(
+          style: IconButton.styleFrom(
+            backgroundColor: isLightBrightness ? null : scaffoldColorDarkTheme,
+          ),
           onPressed: () {
             Navigator.pop(context);
           },
-          icon: Icon(Icons.adaptive.arrow_back, color: Colors.black),
+          icon: Icon(Icons.adaptive.arrow_back),
         ),
         openSearchBar
             ? const ProductSearchField()
             : Row(
                 children: [
                   IconButton(
+                    style: IconButton.styleFrom(
+                      backgroundColor:
+                          isLightBrightness ? null : scaffoldColorDarkTheme,
+                    ),
                     onPressed: () =>
                         ref.read(openSearchBarProvider.notifier).state = true,
-                    icon: Image.asset("assets/icons/search.png", height: 25),
+                    icon: Image.asset(
+                      "assets/icons/search.png",
+                      height: 25,
+                      color: isLightBrightness ? null : Colors.white,
+                    ),
                   ),
                   ShopFavoriteButton(shopID: shop.id),
                   IconButton(
+                    style: IconButton.styleFrom(
+                      backgroundColor:
+                          isLightBrightness ? null : scaffoldColorDarkTheme,
+                    ),
                     onPressed: () => showCallBottomSheet(context, shop.phones!),
                     icon: const Icon(Icons.call, color: Colors.green),
                   ),
