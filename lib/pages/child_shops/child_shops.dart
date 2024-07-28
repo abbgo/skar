975c89ee0461eb781ef79ab214d560f618755658
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-28 07:50:03 +0500

    fix: update ChildShopsPage

--- lib/pages/child_shops/child_shops.dart
+++ lib/pages/child_shops/child_shops.dart
@@ -1,26 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:skar/pages/search_shop/parts/search_field.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:skar/providers/pages/child_shops.dart';
 
-class ChildShopsPage extends StatelessWidget {
+class ChildShopsPage extends ConsumerWidget {
   const ChildShopsPage({super.key, required this.parentShopID});
 
   final String parentShopID;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    var lang = AppLocalizations.of(context)!;
+
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: SearchField(
-          onPressed: onPressed,
-          onSubmitted: onSubmitted,
-          autofocus: autofocus,
-          hintText: hintText,
-          initText: initText,
+          onPressed: () {
+            ref.read(childShopsSearchProvider.notifier).state = '';
+            ref.read(hasChildShopsProvider.notifier).state = true;
+          },
+          onSubmitted: (value) {
+            ref.read(childShopsSearchProvider.notifier).state = value;
+            ref.read(hasChildShopsProvider.notifier).state = true;
+          },
+          autofocus: false,
+          hintText: lang.searchShop,
+          initText: '',
           ref: ref,
         ),
       ),
-      body: Center(
+      body: const Center(
         child: Text('Child shops'),
       ),
     );
