b46619a3d3e914ce25989f7dfe0ceb0f670ccaf3
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-29 12:25:20 +0500

    feat: add childShopsScrollControllerProvider and openChildShopsProductNavigateToTopButtonProvider providers

--- lib/providers/pages/child_shops.dart
+++ lib/providers/pages/child_shops.dart
@@ -1,4 +1,25 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 var childShopsSearchProvider = StateProvider.autoDispose<String>((ref) => '');
 var hasChildShopsProvider = StateProvider.autoDispose<bool>((ref) => true);
+
+var childShopsScrollControllerProvider =
+    StateProvider.autoDispose<ScrollController>((ref) {
+  final controller = ScrollController();
+  controller.addListener(() {
+    if (controller.position.pixels > 1000) {
+      ref
+          .read(openChildShopsProductNavigateToTopButtonProvider.notifier)
+          .state = true;
+    } else {
+      ref
+          .read(openChildShopsProductNavigateToTopButtonProvider.notifier)
+          .state = false;
+    }
+  });
+  return controller;
+});
+
+var openChildShopsProductNavigateToTopButtonProvider =
+    StateProvider.autoDispose<bool>((ref) => false);
