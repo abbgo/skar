9f926be795779c540c1e88731411073cf28f92c6
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-31 05:15:46 +0500

    feat: add MapTypeButton part widget

--- lib/pages/map/parts/map_buttons.dart
+++ lib/pages/map/parts/map_buttons.dart
@@ -1,37 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:skar/helpers/functions.dart';
+import 'package:skar/pages/map/parts/map_type_button.dart';
 import 'package:skar/pages/map/parts/search_shop_button.dart';
-import 'package:skar/providers/pages/map.dart';
-import 'package:skar/styles/colors.dart';
 
 class MapButtons extends ConsumerWidget {
   const MapButtons({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    bool isLightBrightness = isLightTheme(context, ref);
-
     return Positioned(
       top: screenProperties(context).topSafeArea + 5,
       right: 10,
-      child: Column(
+      child: const Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          const SearchShopButton(),
-          Consumer(
-            builder: (context, ref, child) {
-              return IconButton(
-                onPressed: () =>
-                    ref.read(isHybridMapProvider.notifier).change(),
-                icon: Icon(
-                  Icons.layers,
-                  color: isLightBrightness ? elevatedButtonColor : Colors.white,
-                  size: 22,
-                ),
-              );
-            },
-          ),
+          SearchShopButton(),
+          MapTypeButton(),
         ],
       ),
     );
---
+++ lib/pages/map/parts/map_type_button.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:skar/helpers/functions.dart';
+import 'package:skar/providers/pages/map.dart';
+import 'package:skar/styles/colors.dart';
+
+class MapTypeButton extends ConsumerWidget {
+  const MapTypeButton({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    bool isLightBrightness = isLightTheme(context, ref);
+
+    return IconButton(
+      onPressed: () => ref.read(isHybridMapProvider.notifier).change(),
+      icon: Icon(
+        Icons.layers,
+        color: isLightBrightness ? elevatedButtonColor : Colors.white,
+        size: 22,
+      ),
+    );
+  }
+}
