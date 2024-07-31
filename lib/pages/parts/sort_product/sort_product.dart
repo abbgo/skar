07a5e0e8f2858c432fbba09dfe471389ecb8ed14
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-07-31 11:48:02 +0500

    feat: add sortProductPriceProvider provider and update SortProduct part widget

--- lib/pages/parts/sort_product/sort_product.dart
+++ lib/pages/parts/sort_product/sort_product.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:skar/providers/pages/sort_and_filter_product.dart';
 
-class SortProduct extends StatelessWidget {
+class SortProduct extends ConsumerWidget {
   const SortProduct({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    String sortProductPrice = ref.watch(sortProductPriceProvider);
+
     return Container(
       margin: const EdgeInsets.all(20),
       decoration: const BoxDecoration(
         color: Colors.transparent,
         borderRadius: BorderRadius.all(Radius.circular(20)),
       ),
-      child: const Wrap(
-        children: [],
+      child: Wrap(
+        children: [
+          RadioListTile(
+            value: '0-1',
+            groupValue: sortProductPrice,
+            onChanged: (value) =>
+                ref.read(sortProductPriceProvider.notifier).state = value!,
+          ),
+          RadioListTile(
+            value: '1-0',
+            groupValue: sortProductPrice,
+            onChanged: (value) =>
+                ref.read(sortProductPriceProvider.notifier).state = value!,
+          ),
+        ],
       ),
     );
   }
---
+++ lib/providers/pages/sort_and_filter_product.dart
@@ -0,0 +1,3 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final sortProductPriceProvider = StateProvider.autoDispose<String>((ref) => '');
