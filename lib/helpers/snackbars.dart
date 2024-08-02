abf73db3952ab6eb485e003b7e92585806b1bac4
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-02 13:35:28 +0500

    fix: update ShippingButton part widget

--- lib/helpers/snackbars.dart
+++ lib/helpers/snackbars.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:skar/helpers/functions.dart';
-import 'package:skar/styles/colors.dart';
 
 void showIntConnErr(BuildContext context) {
   var snackBar = SnackBar(
@@ -11,28 +9,6 @@ void showIntConnErr(BuildContext context) {
     ),
     duration: const Duration(seconds: 3),
     backgroundColor: Colors.red,
-  );
-
-  ScaffoldMessenger.of(context).showSnackBar(snackBar);
-}
-
-void showTopSnackbar(
-  BuildContext context,
-  String text,
-) {
-  SnackBar snackBar = SnackBar(
-    content: Text(
-      text,
-      style: const TextStyle(color: Colors.white),
-    ),
-    backgroundColor: elevatedButtonColor,
-    dismissDirection: DismissDirection.up,
-    behavior: SnackBarBehavior.floating,
-    margin: EdgeInsets.only(
-      bottom: screenProperties(context).height - 150,
-      left: 10,
-      right: 10,
-    ),
   );
 
   ScaffoldMessenger.of(context).showSnackBar(snackBar);
--- lib/pages/shop/parts/shipping_button.dart
+++ lib/pages/shop/parts/shipping_button.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:skar/helpers/snackbars.dart';
 import 'package:skar/styles/colors.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class ShippingButton extends StatelessWidget {
-  const ShippingButton({
+  ShippingButton({
     super.key,
     required this.hasShipping,
     required this.isLightBrightness,
@@ -13,37 +12,84 @@ class ShippingButton extends StatelessWidget {
   final bool hasShipping;
   final bool isLightBrightness;
 
+  final GlobalKey _tooltipKey = GlobalKey();
+
+  void _showTooltip() {
+    final dynamic tooltip = _tooltipKey.currentState;
+    tooltip?.ensureTooltipVisible();
+  }
+
   @override
   Widget build(BuildContext context) {
     var lang = AppLocalizations.of(context)!;
 
-    return GestureDetector(
-      onTap: () => showTopSnackbar(
-        context,
-        hasShipping ? lang.hasShipping : lang.noShipping,
+    return Tooltip(
+      key: _tooltipKey,
+      message: hasShipping ? lang.hasShipping : lang.noShipping,
+      decoration: ShapeDecoration(
+        shape: const ToolTipCustomDecoration(),
+        color: elevatedButtonColor,
       ),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 10),
-            child: hasShipping
-                ? Image.asset(
-                    "assets/icons/has_shipping.png",
-                    height: 23,
-                  )
-                : Image.asset(
-                    "assets/icons/no_shipping.png",
-                    height: 23,
-                  ),
-          ),
-          Icon(
-            Icons.info,
-            size: 10,
-            color: isLightBrightness ? elevatedButtonColor : Colors.white,
-          ),
-        ],
+      preferBelow: false,
+      verticalOffset: 10,
+      child: GestureDetector(
+        onTap: () => _showTooltip(),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: hasShipping
+                  ? Image.asset(
+                      "assets/icons/has_shipping.png",
+                      height: 23,
+                    )
+                  : Image.asset(
+                      "assets/icons/no_shipping.png",
+                      height: 23,
+                    ),
+            ),
+            Icon(
+              Icons.info,
+              size: 10,
+              color: isLightBrightness ? elevatedButtonColor : Colors.white,
+            ),
+          ],
+        ),
       ),
     );
   }
+}
+
+class ToolTipCustomDecoration extends ShapeBorder {
+  final bool isPaddingRequired;
+
+  const ToolTipCustomDecoration({this.isPaddingRequired = true});
+
+  @override
+  EdgeInsetsGeometry get dimensions =>
+      EdgeInsets.only(bottom: isPaddingRequired ? 20 : 0);
+
+  @override
+  Path getInnerPath(Rect rect, {TextDirection? textDirection}) => Path();
+
+  @override
+  Path getOuterPath(Rect rect, {TextDirection? textDirection}) {
+    rect =
+        Rect.fromPoints(rect.topLeft, rect.bottomRight - const Offset(0, 20));
+
+    return Path()
+      ..addRRect(
+          RRect.fromRectAndRadius(rect, Radius.circular(rect.height / 3)))
+      ..moveTo(rect.bottomCenter.dx - 10, rect.bottomCenter.dy)
+      ..relativeLineTo(10, 20)
+      ..relativeLineTo(10, -20)
+      ..close();
+  }
+
+  @override
+  void paint(Canvas canvas, Rect rect, {TextDirection? textDirection}) {}
+
+  @override
+  ShapeBorder scale(double t) => this;
 }
