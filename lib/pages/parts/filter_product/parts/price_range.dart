ba5d0b927cf02e7b91cc46caf93112764422ee29
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-02 06:30:38 +0500

    fix: update PriceRange part widget

--- lib/pages/parts/filter_product/parts/price_range.dart
+++ lib/pages/parts/filter_product/parts/price_range.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:skar/methods/navigation.dart';
+import 'package:skar/pages/parts/price_range/price_range.dart';
 
 class PriceRange extends StatelessWidget {
   const PriceRange({super.key});
@@ -11,6 +13,13 @@ class PriceRange extends StatelessWidget {
     return ListTile(
       title: Text(lang.priceRange),
       trailing: Icon(Icons.adaptive.arrow_forward),
+      onTap: () => Navigator.push(
+        context,
+        CustomPageRoute(
+          child: const PriceRangePage(),
+          direction: AxisDirection.left,
+        ),
+      ),
     );
   }
 }
