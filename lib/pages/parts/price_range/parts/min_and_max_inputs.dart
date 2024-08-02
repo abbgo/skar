c30653b2ed5ea5218de81298ef8356e879abb8d5
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-02 06:41:59 +0500

    fix: update MinAndMaxInputs part widget

--- lib/pages/parts/price_range/parts/min_and_max_inputs.dart
+++ lib/pages/parts/price_range/parts/min_and_max_inputs.dart
@@ -1,19 +1,48 @@
 import 'package:flutter/material.dart';
 
-class MinAndMaxInputs extends StatelessWidget {
+class MinAndMaxInputs extends StatefulWidget {
   const MinAndMaxInputs({super.key});
 
   @override
+  State<MinAndMaxInputs> createState() => _MinAndMaxInputsState();
+}
+
+class _MinAndMaxInputsState extends State<MinAndMaxInputs> {
+  final TextEditingController minPriceCtrl = TextEditingController();
+  final TextEditingController maxPriceCtrl = TextEditingController();
+
+  @override
+  void dispose() {
+    minPriceCtrl.dispose();
+    maxPriceCtrl.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Row(
-      children: [
-        Expanded(
-          child: TextField(),
-        ),
-        Expanded(
-          child: TextField(),
-        ),
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: minPriceCtrl,
+              decoration: const InputDecoration(
+                hintText: ' In pes baha ',
+              ),
+            ),
+          ),
+          const SizedBox(width: 20),
+          Expanded(
+            child: TextField(
+              controller: maxPriceCtrl,
+              decoration: const InputDecoration(
+                hintText: ' In yokary baha ',
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
