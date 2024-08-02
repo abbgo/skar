d65cb68badd4adf75a83562b04fd41f33a3a44b5
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-02 06:43:28 +0500

    feat: add minPriceProvider and maxPriceProvider prividers

--- lib/providers/pages/sort_and_filter_product.dart
+++ lib/providers/pages/sort_and_filter_product.dart
@@ -1,3 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final sortProductPriceProvider = StateProvider<String>((ref) => '');
+final minPriceProvider = StateProvider.autoDispose<String>((ref) => '');
+final maxPriceProvider = StateProvider.autoDispose<String>((ref) => '');
