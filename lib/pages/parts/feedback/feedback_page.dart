c000bbba39799eb9f01bbbae1b389256f4960499
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-03 12:52:57 +0500

    fix: update FeedbackPage

--- lib/pages/parts/feedback/feedback_page.dart
+++ lib/pages/parts/feedback/feedback_page.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:skar/helpers/functions.dart';
+import 'package:skar/helpers/static_data.dart';
+import 'package:skar/models/complaint.dart';
+import 'package:skar/pages/parts/error.dart';
+import 'package:skar/providers/api/complaint.dart';
+import 'package:skar/providers/local_storadge/setting.dart';
+import 'package:skar/services/complaint.dart';
 import 'package:skar/styles/colors.dart';
 
 class FeedbackPage extends ConsumerWidget {
@@ -9,6 +15,8 @@ class FeedbackPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     bool isLightBrightness = isLightTheme(context, ref);
+    AsyncValue<ResultComplaint> complaints = ref.watch(fetchComplaintsProvider);
+    bool isTM = ref.watch(langProvider) == 'tr';
 
     return Container(
       margin: const EdgeInsets.all(20),
@@ -16,7 +24,24 @@ class FeedbackPage extends ConsumerWidget {
         color: isLightBrightness ? Colors.white : dialogColorDarkTheme,
         borderRadius: const BorderRadius.all(Radius.circular(20)),
       ),
-      child: const Wrap(children: []),
+      child: complaints.when(
+        data: (response) {
+          if (response.error != '') {
+            return const SomeThingWrong();
+          }
+
+          List<Complaint> complaints = response.complaints!;
+          return ListView.builder(
+            itemCount: complaints.length,
+            itemBuilder: (context, index) {
+              Complaint complaint = complaints[index];
+              return Text(isTM ? complaint.textTM : complaint.textRU);
+            },
+          );
+        },
+        error: (error, stackTrace) => errorMethod(error),
+        loading: () => loadWidget,
+      ),
     );
   }
 }
