d72807085def1d78f50e59291f4892dabe1fa24b
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-03 06:42:07 +0500

    feat: add searchProductPriceRangeProvider provider

--- lib/providers/pages/sort_and_filter_product.dart
+++ lib/providers/pages/sort_and_filter_product.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final sortProductPriceProvider = StateProvider<String>((ref) => '');
+final searchProductPriceRangeProvider = StateProvider<String>((ref) => '0-0');
 final priceRangeProvider = StateProvider<String>((ref) => '0-0');
