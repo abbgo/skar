b652b5c6b45ede72dc5258572824d27d0d7e805d
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-04 11:40:14 +0500

    feat: add ComplaintProduct model

---
+++ lib/models/complaint_product.dart
@@ -0,0 +1,16 @@
+class ComplaintProduct {
+  final String complaintID, productID;
+
+  const ComplaintProduct({
+    required this.complaintID,
+    required this.productID,
+  });
+
+  factory ComplaintProduct.defaultComplaintProduct() {
+    return const ComplaintProduct(complaintID: '', productID: '');
+  }
+
+  Map<String, dynamic> toMap() {
+    return {'complaint_id': complaintID, 'product_id': productID};
+  }
+}
