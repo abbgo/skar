2bbec24a44f9182c0baa045fb7d003ff66bbd5d8
allanurbayramgeldiyev209 <allanurbayramgeldiyev209@gmail.com>
2024-08-07 10:35:00 +0500

    fix: update ShopListCardImage part widget

--- lib/pages/parts/shop_list_card/parts/shop_list_card_image.dart
+++ lib/pages/parts/shop_list_card/parts/shop_list_card_image.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:skar/methods/navigation.dart';
 import 'package:skar/methods/parts/image.dart';
 import 'package:skar/models/shop.dart';
@@ -14,32 +13,11 @@ class ShopListCardImage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Expanded(
       flex: 2,
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: const BorderRadius.all(Radius.circular(10)),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.1),
-              spreadRadius: 3,
-              blurRadius: 30,
-              offset: const Offset(0, 4),
-            )
-          ],
-        ),
-        width: double.infinity,
-        height: double.infinity,
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(10),
-          child: Consumer(
-            builder: (context, ref, child) {
-              return GestureDetector(
-                onTap: () =>
-                    goToPage(context, ShopPage(shopID: shop.id!), false),
-                child: showCachImageMethod(shop.image!),
-              );
-            },
-          ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: GestureDetector(
+          onTap: () => goToPage(context, ShopPage(shopID: shop.id!), false),
+          child: showCachImageMethod(shop.image!),
         ),
       ),
     );
