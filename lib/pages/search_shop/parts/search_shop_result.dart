7fbf81af5c40badfe8c17db80d9a21360217fdc4
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-08-27 14:25:39 +0500

    fix: update SearchShopResult part widget

--- lib/pages/search_shop/parts/search_shop_result.dart
+++ lib/pages/search_shop/parts/search_shop_result.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:skar/datas/static.dart';
-import 'package:skar/helpers/functions.dart';
 import 'package:skar/helpers/static_data.dart';
 import 'package:skar/models/shop.dart';
 import 'package:skar/pages/parts/error.dart';
@@ -22,46 +21,47 @@ class SearchShopResult extends ConsumerWidget {
     ScrollController scrollController =
         ref.watch(searchShopScrollControllerProvider);
 
-    return !hasShops
-        ? const NoResult()
-        : Container(
-            margin: const EdgeInsets.symmetric(horizontal: 10),
-            height: screenProperties(context).height,
-            child: ListView.builder(
-              controller: scrollController,
-              physics: const BouncingScrollPhysics(),
-              itemBuilder: (context, index) {
-                final page = index ~/ pageSize + 1;
-                final indexInPage = index % pageSize;
+    if (!hasShops) {
+      return const NoResult();
+    } else {
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: ListView.builder(
+          controller: scrollController,
+          physics: const BouncingScrollPhysics(),
+          itemBuilder: (context, index) {
+            final page = index ~/ pageSize + 1;
+            final indexInPage = index % pageSize;
 
-                ShopParams shopParams = ShopParams(page: page, isRandom: false);
+            ShopParams shopParams = ShopParams(page: page, isRandom: false);
 
-                final AsyncValue<ResultShop> responseAsync =
-                    ref.watch(fetchShopsProvider(shopParams));
+            final AsyncValue<ResultShop> responseAsync =
+                ref.watch(fetchShopsProvider(shopParams));
 
-                return responseAsync.when(
-                  skipLoadingOnRefresh: true,
-                  skipLoadingOnReload: true,
-                  skipError: true,
-                  data: (response) {
-                    if (response.error != '') {
-                      return null;
-                    }
-                    if (indexInPage >= response.shops!.length) {
-                      return null;
-                    }
-                    final shop = response.shops![indexInPage];
-                    return ShopListTile(
-                      shop: shop,
-                      mapPageContext: mapPageContext,
-                      forFavorite: false,
-                    );
-                  },
-                  error: (error, stackTrace) => errorMethod(error),
-                  loading: () => null,
+            return responseAsync.when(
+              skipLoadingOnRefresh: true,
+              skipLoadingOnReload: true,
+              skipError: true,
+              data: (response) {
+                if (response.error != '') {
+                  return null;
+                }
+                if (indexInPage >= response.shops!.length) {
+                  return null;
+                }
+                final shop = response.shops![indexInPage];
+                return ShopListTile(
+                  shop: shop,
+                  mapPageContext: mapPageContext,
+                  forFavorite: false,
                 );
               },
-            ),
-          );
+              error: (error, stackTrace) => errorMethod(error),
+              loading: () => null,
+            );
+          },
+        ),
+      );
+    }
   }
 }
