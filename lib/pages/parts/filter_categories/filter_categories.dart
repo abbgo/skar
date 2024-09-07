4809772e4303603d5726882f017e2fe97e48b93c
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-09-07 17:31:54 +0500

    fix: update FilterCategoriesPage

--- lib/pages/parts/filter_categories/filter_categories.dart
+++ lib/pages/parts/filter_categories/filter_categories.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class FilterCategoriesPage extends StatelessWidget {
   const FilterCategoriesPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var lang = AppLocalizations.of(context)!;
+
     return Scaffold(
-        appBar: AppBar(
-      title: const Text('Kategoriyalar'),
-    ));
+      appBar: AppBar(
+        title: Text(lang.categories),
+      ),
+    );
   }
 }
