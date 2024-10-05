dd8bdd228a959744e12145aad3c7ba8addb1f433
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-05 10:49:57 +0500

    feat: add FilterGendersPage

--- lib/pages/parts/filter_product/parts/filter_gender_button.dart
+++ lib/pages/parts/filter_product/parts/filter_gender_button.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:skar/methods/navigation.dart';
+import 'package:skar/pages/filter_genders/filter_genders.dart';
 
 class FilterGenderButton extends StatelessWidget {
   const FilterGenderButton({super.key});
@@ -8,7 +10,13 @@ class FilterGenderButton extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.only(left: 15, right: 25, top: 15, bottom: 10),
       child: GestureDetector(
-        onTap: () {},
+        onTap: () => Navigator.push(
+          context,
+          CustomPageRoute(
+            child: const FilterGendersPage(),
+            direction: AxisDirection.left,
+          ),
+        ),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
