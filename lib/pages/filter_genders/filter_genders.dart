05a12947ff902388360d4d891a998399018d42cf
Mac OS <allanurbayramgeldiyev109@gmail.com>
2024-10-05 13:10:16 +0500

    fix: update FilterGendersPage

--- lib/pages/filter_genders/filter_genders.dart
+++ lib/pages/filter_genders/filter_genders.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:skar/datas/static.dart';
 import 'package:skar/pages/filter_genders/parts/gender_checkbox_list_tile.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:skar/pages/parts/filter_categories/parts/filter_categories_save_button.dart';
 
 class FilterGendersPage extends StatelessWidget {
   const FilterGendersPage({super.key, required this.forSearchProduct});
@@ -16,21 +17,28 @@ class FilterGendersPage extends StatelessWidget {
       appBar: AppBar(title: const Text('Jynsy')),
       body: Column(
         children: [
-          GenderCheckboxListTile(
-            title: lang.male,
-            gender: Genders.male,
-            forSearchProduct: forSearchProduct,
+          Expanded(
+            child: Column(
+              children: [
+                GenderCheckboxListTile(
+                  title: lang.male,
+                  gender: Genders.male,
+                  forSearchProduct: forSearchProduct,
+                ),
+                GenderCheckboxListTile(
+                  title: lang.female,
+                  gender: Genders.female,
+                  forSearchProduct: forSearchProduct,
+                ),
+                GenderCheckboxListTile(
+                  title: lang.child,
+                  gender: Genders.child,
+                  forSearchProduct: forSearchProduct,
+                ),
+              ],
+            ),
           ),
-          GenderCheckboxListTile(
-            title: lang.female,
-            gender: Genders.female,
-            forSearchProduct: forSearchProduct,
-          ),
-          GenderCheckboxListTile(
-            title: lang.child,
-            gender: Genders.child,
-            forSearchProduct: forSearchProduct,
-          ),
+          const FilterCategoriesSaveButton(),
         ],
       ),
     );
--- lib/pages/parts/filter_categories/parts/filter_categories_save_button.dart
+++ lib/pages/parts/filter_categories/parts/filter_categories_save_button.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:skar/helpers/functions.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
@@ -13,13 +12,9 @@ class FilterCategoriesSaveButton extends StatelessWidget {
     return Container(
       width: screenProperties(context).width,
       padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
-      child: Consumer(
-        builder: (context, ref, child) {
-          return ElevatedButton(
-            onPressed: () => Navigator.pop(context),
-            child: Text(lang.select),
-          );
-        },
+      child: ElevatedButton(
+        onPressed: () => Navigator.pop(context),
+        child: Text(lang.select),
       ),
     );
   }
